215d45f229ba9894853e09aa9a3abedcf3e0d0e4
protonn <support@argentum.io>
2017-02-06 21:31:02 -0600

    block explorer

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -62,7 +62,7 @@ void OptionsModel::Init()
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "http://insight-myr.cryptap.us/tx/%s");
+        settings.setValue("strThirdPartyTxUrls", "https://chainz.cryptoid.info/arg/tx.dws?%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
