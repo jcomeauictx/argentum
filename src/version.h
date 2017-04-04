65243ee2b016c1a465f2f74f84c0547918018792
protonn <support@argentum.io>
2017-04-03 21:39:21 -0500

    MEMPOOL_GD_VERSION

--- src/version.h
+++ src/version.h
@@ -32,6 +32,6 @@ static const int NOBLKS_VERSION_END = 32400;
 static const int BIP0031_VERSION = 60000;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 1040000;
 
 #endif // BITCOIN_VERSION_H
