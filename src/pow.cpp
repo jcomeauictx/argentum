9521e5fea39d1faa5b96c3078694ccff813383d5
protonn <support@argentum.io>
2017-04-05 23:20:13 -0500

    network adjustments

--- src/pow.cpp
+++ src/pow.cpp
@@ -503,11 +503,10 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     // Check proof of work matches claimed amount
     if (nHeight > params.nCoinbaseMaturityV2Start){
         if (UintToArith256(hash) > bnTarget)
-            return error("CheckProofOfWork1(): hash doesn't match nBits, nHeight=%d", nHeight);}
+            return error("CheckProofOfWork(): hash doesn't match nBits");}
 
-    if (nHeight >= params.nCheckProof && nHeight < params.nCheckProof1){
-        if (UintToArith256(hash) > bnTarget)
-            return error("CheckProofOfWork2(): hash doesn't match nBits, nHeight=%d", nHeight);}
+        /*if (UintToArith256(hash) > bnTarget)
+            return error("CheckProofOfWork(): hash doesn't match nBits");*/
 
     return true;
 }
