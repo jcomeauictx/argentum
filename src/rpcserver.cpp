4824de837ce959f5e904a49f430814056a6490cf
protonn <support@argentum.io>
2017-04-14 10:34:22 -0500

    enable rpc call getnetworkhashps
    I don't know how this got disabled /facepalm

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -302,7 +302,7 @@ static const CRPCCommand vRPCCommands[] =
     /* Mining */
     { "mining",             "getblocktemplate",       &getblocktemplate,       true  },
     { "mining",             "getmininginfo",          &getmininginfo,          true  },
-//    { "mining",             "getnetworkhashps",       &getnetworkhashps,       true  },
+    { "mining",             "getnetworkhashps",       &getnetworkhashps,       true  },
     { "mining",             "prioritisetransaction",  &prioritisetransaction,  true  },
     { "mining",             "submitblock",            &submitblock,            true  },
 #ifdef ENABLE_WALLET
