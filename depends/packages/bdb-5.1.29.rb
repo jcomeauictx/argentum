09df4c277092568db2b8e3a062e3eed4e7edbfb5
protonn <support@argentum.io>
2017-04-16 01:35:01 -0500

    add brew forumla for bdb 5.1.29 [osx]

---
+++ depends/packages/bdb-5.1.29.rb
@@ -0,0 +1,69 @@
+require 'formula'
+
+class Bdb5129 < Formula
+  homepage 'http://www.oracle.com/technology/products/berkeley-db/index.html'
+  url 'http://download.oracle.com/berkeley-db/db-5.1.29.tar.gz'
+  sha256 'a943cb4920e62df71de1069ddca486d408f6d7a09ddbbb5637afe7a229389182'
+
+  bottle do
+    cellar :any
+    sha256 "82b52acb094e7f5cfb8f807f4501b47653744285" => :mavericks
+    sha256 "32db77b55f8f864af939d842a85bed64347912e8" => :mountain_lion
+    sha256 "6846c61e17ff5dcfe25f5aaf2e7e88b71758ea09" => :lion
+  end
+
+
+  # Fix build under Xcode 4.6
+  # Double-underscore names are reserved, and __atomic_compare_exchange is now
+  # a built-in, so rename this to something non-conflicting.
+  patch :DATA
+
+  def install
+    # BerkeleyDB dislikes parallel builds
+    ENV.deparallelize
+    # --enable-compat185 is necessary because our build shadows
+    # the system berkeley db 1.x
+    args = %W[
+      --disable-replication
+      --disable-shared
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --enable-cxx
+      --enable-compat185
+    ]
+
+    # BerkeleyDB requires you to build everything from the build_unix subdirectory
+    cd 'build_unix' do
+      system "../dist/configure", *args
+      system "make install"
+
+      # use the standard docs location
+      doc.parent.mkpath
+      mv prefix/'docs', doc
+    end
+  end
+end
+
+__END__
+diff --git a/src/dbinc/atomic.h b/src/dbinc/atomic.h
+index 1a96e2a..f318576 100644
+--- a/src/dbinc/atomic.h
++++ b/src/dbinc/atomic.h
+@@ -144,7 +144,7 @@ typedef LONG volatile *interlocked_val;
+ #define  atomic_inc(env, p)  __atomic_inc(p)
+ #define  atomic_dec(env, p)  __atomic_dec(p)
+ #define  atomic_compare_exchange(env, p, o, n) \
+- __atomic_compare_exchange((p), (o), (n))
++ __atomic_compare_exchange_db((p), (o), (n))
+ static inline int __atomic_inc(db_atomic_t *p)
+ {
+  int temp;
+@@ -176,7 +176,7 @@ static inline int __atomic_dec(db_atomic_t *p)
+  * http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+  * which configure could be changed to use.
+  */
+-static inline int __atomic_compare_exchange(
++static inline int __atomic_compare_exchange_db(
+  db_atomic_t *p, atomic_value_t oldval, atomic_value_t newval)
+ {
+  atomic_value_t was;
