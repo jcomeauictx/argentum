b64aef5ac2e2bb4267c08271cc8afd3dd69a47d5
John Comeau <jc@unternet.net>
2018-01-25 18:13:48 -0500

    always forget newlines.

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -134,7 +134,7 @@ bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs)
         // get the scriptPubKey corresponding to this input:
         const CScript& prevScript = prev.scriptPubKey;
         if (!Solver(prevScript, whichType, vSolutions)) {
-            LogPrintf("AreInputsStandard fails at prev.scriptPubKey at input %d", i);
+            LogPrintf("AreInputsStandard fails at prev.scriptPubKey at input %d\n", i);
             return false;
         }
 
@@ -143,16 +143,16 @@ bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs)
             std::vector<std::vector<unsigned char> > stack;
             // convert the scriptSig into a stack, so we can inspect the redeemScript
             if (!EvalScript(stack, tx.vin[i].scriptSig, SCRIPT_VERIFY_NONE, BaseSignatureChecker(), SIGVERSION_BASE)) {
-                LogPrintf("AreInputsStandard fails at EvalScript(stack, tx.vin[%d].scriptSig)", i);
+                LogPrintf("AreInputsStandard fails at EvalScript(stack, tx.vin[%d].scriptSig)\n", i);
                 return false;
             }
             if (stack.empty()) {
-                LogPrintf("AreInputsStandard fails at stack empty at index %d", i);
+                LogPrintf("AreInputsStandard fails at stack empty at index %d\n", i);
                 return false;
             }
             CScript subscript(stack.back().begin(), stack.back().end());
             if (subscript.GetSigOpCount(true) > MAX_P2SH_SIGOPS) {
-                LogPrintf("AreInputsStandard fails at MAX_P2SH_SIGOPS at index %d", i);
+                LogPrintf("AreInputsStandard fails at MAX_P2SH_SIGOPS at index %d\n", i);
                 return false;
             }
         }
